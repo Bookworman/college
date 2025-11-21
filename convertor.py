cca2c022cf0c684f83e0e1634c506b731f7f36f3
Bookworman <Incens@yandex.ru>
2025-11-21 03:22:09 +0300

    Create convertor.py

---
+++ convertor.py
@@ -0,0 +1,15 @@
+
+def convert_usd_to_rub (amount_usd):
+
+    """Конвертирует введенную сумму $ в руб
+    args:
+        amount_usd: float
+    returns:
+        float: сумма руб"""
+
+    usd_to_rub = 95.50
+    amount_rub = amount_usd * usd_to_rub
+    return amount_rub
+
+amount_usd = float(input("Введите кол-во $ "))
+print(convert_usd_to_rub(amount_usd))
